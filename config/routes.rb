7f00671cd79e1aa39f7eeb9e812be9b5f23456f3
amarkpark <amarkpark@gmail.com>
2015-09-01 13:56:22 +0000

    Lesson 4: created controller, root index route, index.html.erb. Page currently loads. ~AMP

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Nomster::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'places#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
