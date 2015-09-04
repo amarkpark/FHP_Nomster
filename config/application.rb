2b10ff20057aa2ae56f9abb13aca651b10048005
amarkpark <amarkpark@gmail.com>
2015-09-04 21:59:07 +0000

    Lesson 11: added a web font. ~AMP

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module Nomster
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
+
   end
 end
