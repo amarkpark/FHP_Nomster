bad35fd6358ab1774552a43b7ff80dae1c8a36b2
amarkpark <amarkpark@gmail.com>
2015-09-05 22:42:42 +0000

    Lesson 14: Added DB.create functionality to the new record page. Button submits record and redirects to index page. ~AMP

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,19 @@ class PlacesController < ApplicationController
 	end
 	# end added for pagination
 
+	# PLEASE NOTE the variable in the new action is SINGULAR, ONE RECORD
 	def new
-		@places = Place.new
+		@place = Place.new
+	end
+
+	# Note the use of the singular variable "place" here to create one record
+	def create
+		Place.create(places_params)
+		redirect_to root_path
+	end
+
+	def places_params
+		params.require(:place).permit(:name, :description, :address)
 	end
 
 end
