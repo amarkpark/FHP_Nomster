b02ccb67c9a0999a34bed33db647811435e46b66
amarkpark <amarkpark@gmail.com>
2015-09-08 03:12:08 +0000

    Lesson 28: Validations work but DON'T fail gracefully! need to fix that ASAP! No way to Cancel edit or update and bad data has crashed App. ~AMP

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,6 +10,8 @@ class Place < ActiveRecord::Base
 
 	# Adding association to Users table
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :length {:minimum => 3}
+	validates :address, :presence => true
+	validates :description, :presence => true, :length {:minimum => 5}
 
 end
