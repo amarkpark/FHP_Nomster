6cc154c1a54fd17980eabad04bba747c9d0cec4b
amarkpark <amarkpark@gmail.com>
2015-09-14 16:52:42 +0000

    added userid merge and changed troubleshooting photo output for specific place ~AMP

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,7 +6,7 @@ class PhotosController < ApplicationController
 
 	def create
 		@place = Place.find(params[:place_id])
-		@place.photos.create(photo_params)
+		@place.photos.create(photo_params.merge(:user => current_user))
 		redirect_to place_path(@place)
 	end
 
