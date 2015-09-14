e952ed525c1a01a64a30b4c32682dd509fbad796
amarkpark <amarkpark@gmail.com>
2015-09-14 21:17:18 +0000

    Lesson 42, personalizing email notifications. ~AMP

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "noreply@ampnomsterapp.com"
 
-  def comment_added
-  	mail(to: "amarkpark@gmail.com", subject: 'testing comment notification')
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+
+  	mail(to: @place_owner.email, subject: "Comment added to #{@place.name}")
   end
 end
