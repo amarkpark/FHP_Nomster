04971042fa5c5656ad0be3118658695c125a35e5
amarkpark <amarkpark@gmail.com>
2015-09-23 18:24:43 +0000

    TDD9: three functional tests, a little cleanup. ~AMP

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -4,9 +4,7 @@ class CommentsControllerTest < ActionController::TestCase
 
 	test "submit comment" do
 		user = FactoryGirl.create(:user)
-		sign_in user
 		place = FactoryGirl.create(:place, :user => user)
-		sign_out user
 		user2 = FactoryGirl.create(:user)
 		sign_in user2
 		assert_difference 'place.comments.count' do
@@ -14,6 +12,8 @@ class CommentsControllerTest < ActionController::TestCase
 			post :create, :place_id => place.id, :comment => { :rating  => '5_stars', :message => "You spin me round"}
 		end
 		assert_redirected_to place_path(place.id)
+		comment = Comment.last
+		assert_equal user2, comment.user
 	end
 
 end
